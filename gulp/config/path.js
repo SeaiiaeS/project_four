dacf5facb8b9a67c4711287aaf25ac8e3ed4d15d
SeaiiaeS <hayk.adamyan.2002@gmail.com>
2022-05-29 15:23:12 +0400

    dependencies to devdepend and gulp config files

---
+++ gulp/config/path.js
@@ -0,0 +1,24 @@
+import * as nodePath from 'path';
+const rootFolder =nodePath.basename(nodePath.resolve())
+
+const buildFolder= './build';
+const srcFolder= './src';
+
+
+const path = {
+	build: {
+        files: `${buildFolder}/`,
+    },
+	src: {
+        html: `${srcFolder}/index.html`,
+        scss: `${srcFolder}/index.scss`,
+    },
+	watch: {
+        html: `${srcFolder}/**/*.html`,
+        scss: `${srcFolder}/**/*.scss`,
+    },
+	clean: buildFolder,
+	buildFolder: buildFolder,
+	srcFolder: srcFolder,
+	rootFolder: rootFolder
+}
